fe4cc44b2f0ee495ac6af26d17847740149e97c5
Larissa Silva <larissa.cordeiro@idinheiro.com.br>
2022-04-04 16:06:35 -0300

    feat: config sotorybook and create first example

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Default title',
+    description: 'Default description'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+Basic.args = {
+  title: 'Basic',
+  description: 'Basic description'
+}
